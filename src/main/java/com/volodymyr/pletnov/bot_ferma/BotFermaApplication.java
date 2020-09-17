6d53f5ace5273a7d462bd733fad10680e42aa7f8
Vladimir <deadlight967@gmail.com>
2020-09-17 15:08:55 +0300

    first

---
+++ src/main/java/com/volodymyr/pletnov/bot_ferma/BotFermaApplication.java
@@ -0,0 +1,13 @@
+package com.volodymyr.pletnov.bot_ferma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotFermaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotFermaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volodymyr/pletnov/bot_ferma/BotFermaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volodymyr.pletnov.bot_ferma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotFermaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
